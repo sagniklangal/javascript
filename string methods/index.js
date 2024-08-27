b9d373e2d7b77e3cd743cc45cd50831e3ba4b409
sagniklangal <sagniklangal370@gmail.com>
2024-08-27 20:09:21 +0530

    Added string methods folder with new code

---
+++ string methods/index.js
@@ -0,0 +1,82 @@
+//String methods = Allows you to manipulate and work with text(Strings)
+
+let userName = "Langal";
+
+//charAt()
+// let ch = userName.charAt(2)
+// console.log(ch);
+
+//indexOf()
+// let firstOccurence = userName.indexOf('a');
+// console.log(firstOccurence);
+
+//lastIndexOf()
+// let lastOccurence = userName.lastIndexOf('a');
+// console.log(lastOccurence);
+
+//length
+// let stringLength = userName.length;
+// console.log(stringLength);
+
+//trim()
+// let beforeTrim = "    Langal    ";
+// let trimmed = userName.trim();
+// console.log(beforeTrim);
+// console.log(trimmed);
+
+//toUpperCase()
+// let upper = userName.toUpperCase();
+// console.log(upper);
+
+//toLowerCase()
+// let lower = userName.toLowerCase();
+// console.log(lower);
+
+//repeat()
+// let repeat = userName.repeat(3);
+// console.log(repeat);
+
+//startsWith()
+// let st = "vivek";
+// let result = (st.startsWith(" ")); //Checking if starts with space or not?
+// if(result){
+//   console.log("Your username can't start with a space");
+// }
+// else{
+//   console.log(userName);
+// }
+
+//endsWith()
+// let en = "vivek ";
+// let result1 = (en.endsWith(" ")); //Checking if ends with space or not?
+// if(result1){
+//   console.log("Your username can't end with a space");
+// }
+// else{
+//   console.log(userName);
+// }
+
+// includes()
+// let incl = "vivek ";
+// let result2 = (incl.includes(" "));
+// if(result2){
+//   console.log("Your username can't include an extra space");
+// }
+// else{
+//   console.log(userName);
+// }
+
+//replaceAll("","")
+// let phoneNumber = "123-456-7890";
+// phoneNumber = phoneNumber.replaceAll("-","/"); //Replacing "-" with "/"
+// console.log(phoneNumber);
+
+//padStart()
+// let phoneNumber = "123-456-7890";
+// phoneNumber = phoneNumber.padStart(25,"0"); //
+// console.log(phoneNumber);
+
+//padEnd()
+// let phoneNumber = "123-456-7890";
+// phoneNumber = phoneNumber.padEnd(15,"0"); //
+// console.log(phoneNumber);

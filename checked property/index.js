e0c6a51625cd23055cb9999dd010f5c4206b9c01
sagniklangal <sagniklangal370@gmail.com>
2024-08-27 17:41:05 +0530

    Added checked property folder with new code

---
+++ checked property/index.js
@@ -0,0 +1,30 @@
+//.checked = property that determines the checked state of an HTML checkbox or radio button element
+
+const myCheckBox = document.getElementById("myCheckbox");
+const visaBtn = document.getElementById("visaBtn");
+const masterBtn = document.getElementById("masterBtn");
+const paypalBtn = document.getElementById("paypalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function(){
+  if(myCheckBox.checked){
+    subResult.textContent = `You have subscribed!`;
+  }
+  else{
+    subResult.textContent = `You have not subscribed!`;
+  }
+  if (visaBtn.checked) {
+    paymentResult.textContent = `You are paying with Visa`;
+  }
+  else if (mastercardBtn.checked) {
+    paymentResult.textContent = "You are paying with Mastercard";
+  }
+  else if (paypalBtn.checked) {
+    paymentResult.textContent = "You are paying with PayPal";
+  }
+  else{
+    paymentResult.textContent = "You must select a payment type";
+  }
+}

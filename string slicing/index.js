24954185159c357ce5c298ec88e806d775641a0e
sagniklangal <sagniklangal370@gmail.com>
2024-08-27 20:37:15 +0530

    Added string slicing folder with new code

---
+++ string slicing/index.js
@@ -0,0 +1,29 @@
+//String methods = Allows you to manipulate and work with text(Strings)
+
+// string slicing = Creating a substring from a portion of another string
+// String.slice(start, end);
+
+const fullName = "Sagnik Langal"
+// let firstName = fullName.slice(0, 6);
+// // let lastName = fullName.slice(7, 13);
+// let lastName = fullName.slice(7); // 7 till last
+// let firstChar = fullName.slice(0,1);
+// let lastChar = fullName.slice(-3);
+
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(firstChar);
+// console.log(lastChar);
+
+
+// let firstName = fullName.slice(0, fullName.indexOf(" "));
+// let lastName = fullName.slice(fullName.indexOf(" ")+1);
+// console.log(firstName);
+// console.log(lastName);
+
+// const email = "abc@gmail.com";
+// let userName = email.slice(0, email.indexOf("@"));
+// let extension = email.slice(email.indexOf("@")+1);
+// console.log(userName);
+// console.log(extension);

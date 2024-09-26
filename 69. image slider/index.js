02b37bc98221e1764d28769856989c04188c9e92
sagniklangal <sagniklangal370@gmail.com>
2024-09-26 22:02:58 +0530

    Added image slider

---
+++ 69. image slider/index.js
@@ -0,0 +1,44 @@
+/*
+Image Slider
+*/
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+//initializeSlider();
+
+//The dom content will be loaded first, then we will initialize the slider
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider() {
+  if (slides.length > 0) {
+    slides[slideIndex].classList.add("displaySlide");
+    intervalId = setInterval(nextSlide, 5000);
+  }
+  
+}
+function showSlide(index){
+  if (index >= slides.length) {
+    slideIndex = 0;
+  }
+  else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+  slides.forEach(slide => {
+    slide.classList.remove("displaySlide");
+  });
+  slides[slideIndex].classList.add("displaySlide");
+}
+function prevSlide(){
+  clearInterval(intervalId);
+  slideIndex--;
+  showSlide(slideIndex);
+}
+function nextSlide(){
+  clearInterval(intervalId);
+  slideIndex++;
+  showSlide(slideIndex);
+}
+
+
